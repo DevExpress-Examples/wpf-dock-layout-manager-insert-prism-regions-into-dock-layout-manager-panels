e4181fc9f3b2424b221a8f1e43ebb40732098f66
Pavel Ovcharov (DevExpress) <1357165+pavelovcharov@users.noreply.github.com>
2018-06-07 17:13:29 +0300

    Update

--- CS/Modules.Chart/ChartModule.cs
+++ CS/Modules.Chart/ChartModule.cs
@@ -15,12 +15,8 @@
 // You can find sample updates and versions for different programming languages here:
 // http://www.devexpress.com/example=E1926
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Practices.Prism.Modularity;
-using Microsoft.Practices.Prism.Regions;
+using Prism.Modularity;
+using Prism.Regions;
 
 namespace Modules.Chart {
     public class ChartModule: IModule {
@@ -28,7 +24,7 @@ namespace Modules.Chart {
         public ChartModule(IRegionManager regionManager) {
             this.regionManager = regionManager;
         }
-        public void Initialize() {
+        void IModule.Initialize() {
             regionManager.RegisterViewWithRegion("ChartRegion", typeof(ChartView));
         }
     }
