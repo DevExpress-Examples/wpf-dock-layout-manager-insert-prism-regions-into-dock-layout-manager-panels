6c465478c46778eed0109c92c0f34deee86a56e2
DevExpressExampleBot <fakeEmail>
2022-07-07 12:46:50 +0400

    Source auto update [skip ci]

--- VB/PrismSupportSample/Shell.xaml.vb
+++ VB/PrismSupportSample/Shell.xaml.vb
@@ -1,14 +1,15 @@
-﻿Imports DevExpress.Xpf.Core
+Imports DevExpress.Xpf.Core
 
 Namespace PrismSupportSample
+
     ''' <summary>
     ''' Interaction logic for Window1.xaml
     ''' </summary>
-    Partial Public Class Shell
+    Public Partial Class Shell
         Inherits DXWindow
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
     End Class
 End Namespace
