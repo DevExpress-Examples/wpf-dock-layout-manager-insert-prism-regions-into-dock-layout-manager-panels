2f153ae5935e7237300cebf808adc239c51e7ff5
Andrey Marten <andrey.kunitsyn@devexpress.com>
2026-04-24 14:23:45 +0200

    migrate to 26.1, .NET 10, Prism 9 in CS

--- CS/Modules.Main/MainView.xaml.cs
+++ CS/Modules.Main/MainView.xaml.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Linq;
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
 
 namespace Modules.Main {
-    /// <summary>
-    /// Interaction logic for MainView.xaml
-    /// </summary>
     public partial class MainView : UserControl {
         public MainView() {
             InitializeComponent();
--- CS/PrismSupportSample/App.xaml.cs
+++ CS/PrismSupportSample/App.xaml.cs
@@ -1,16 +1,29 @@
-﻿using System;
-using System.Linq;
+﻿using DevExpress.Xpf.Docking;
+using DevExpress.Xpf.Prism;
+using Modules.Main;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Navigation.Regions;
+using Prism.Unity;
 using System.Windows;
 
 namespace PrismSupportSample {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application {
-        protected override void OnStartup(StartupEventArgs e) {
-            base.OnStartup(e);
-            Bootstrapper bootstrapper = new Bootstrapper();
-            bootstrapper.Run();
+    public partial class App : PrismApplication {
+        protected override Window CreateShell() {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry) {
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog) {
+            moduleCatalog.AddModule<MainModule>();
+        }
+
+        protected override void ConfigureRegionAdapterMappings(RegionAdapterMappings regionAdapterMappings) {
+            base.ConfigureRegionAdapterMappings(regionAdapterMappings);
+            var factory = Container.Resolve<IRegionBehaviorFactory>();
+            regionAdapterMappings.RegisterMapping(typeof(LayoutPanel), AdapterFactory.Make<RegionAdapterBase<LayoutPanel>>(factory));
         }
     }
 }
